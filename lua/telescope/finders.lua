742a5137a0a62a8dd106a32484b9a6293cb45c9d
smolck <46855713+smolck@users.noreply.github.com>
2020-08-30 14:52:23 -0500

    Revert finders.lua change

--- lua/telescope/finders.lua
+++ lua/telescope/finders.lua
@@ -129,8 +129,6 @@ function Finder:_find(prompt, process_result, process_complete)
 
     maximum_results = self.maximum_results,
 
-    writer = opts.writer and Job:new(opts.writer) or nil,
-
     on_stdout = on_output,
     on_stderr = on_output,
 
