0e54e295047e092044d57b0837564f1ebb439789
Senghan Bright <senghan.bright@deltaprojects.com>
2020-09-23 17:51:27 +0200

    feature: Vim help-tags picker

--- lua/telescope/make_entry.lua
+++ lua/telescope/make_entry.lua
@@ -284,4 +284,46 @@ function make_entry.gen_from_treesitter(opts)
   end
 end
 
+function make_entry.gen_from_tagfile(opts)
+    local help_entry = ""
+    local version = ""
+    local delim = string.char(7)
+
+    local make_display = function(line)
+      line = line .. delim
+      for section in line:gmatch("(.-)" .. delim) do
+        if section:find("^vim:") == nil then
+          local ver = section:match("^neovim:(.*)")
+          if ver == nil then
+            help_entry = section
+          else
+            version = ver:sub(1, -2)
+          end
+        end
+      end
+      if version ~= "" then -- some Vim only entries are unversioned
+        if opts.show_version then
+          display = string.format("%s [%s]", help_entry, version)
+        else
+          display = help_entry
+        end
+      end
+
+      return display
+    end
+
+    return function(line)
+      local entry = {
+        valid = help_entry ~= "",
+        entry_type = make_entry.types.GENERIC,
+
+        value = help_entry,
+        ordinal = help_entry,
+      }
+      entry.display = make_display(line)
+
+      return entry
+    end
+end
+
 return make_entry
