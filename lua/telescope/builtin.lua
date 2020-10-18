69563c5247dd70220bc511591a58033760269368
Malcolm Ramsay <malramsay64@gmail.com>
2020-10-19 08:26:02 +1030

    fix: Ensure git_files uses the git root directory as cwd
    This sets the cwd option of the git_files builtin to use the root of the
    git directory when the cwd option doesn't already exist. When git lists
    files, it is relative to the root of the git directory, rather than the
    current working directory. This caused problems when using git_files in
    a subdirectory of the git root (see #174).
    
    This commit fixes the issue by always setting the cwd as the root of the
    git directory.

--- lua/telescope/builtin.lua
+++ lua/telescope/builtin.lua
@@ -46,10 +46,14 @@ local builtin = {}
 builtin.git_files = function(opts)
   opts = opts or {}
 
-  opts.entry_maker = opts.entry_maker or make_entry.gen_from_file(opts)
   if opts.cwd then
     opts.cwd = vim.fn.expand(opts.cwd)
+  else
+    opts.cwd = utils.capture("git rev-parse --show-toplevel")
   end
+  -- By creating the entry maker after the cwd options,
+  -- we ensure the maker uses the cwd options when being created.
+  opts.entry_maker = opts.entry_maker or make_entry.gen_from_file(opts)
 
   pickers.new(opts, {
     prompt_title = 'Git File',
