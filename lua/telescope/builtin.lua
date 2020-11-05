7f0a81fab96c09eeeddb8d627717e89bc026dba8
Emmanuel Thompson <eet6646@gmail.com>
2020-11-05 14:09:36 -0500

    Add vimspector picker

--- lua/telescope/builtin.lua
+++ lua/telescope/builtin.lua
@@ -904,4 +904,39 @@ builtin.marks = function(opts)
   }):find()
 end
 
+builtin.vimspector = function(opts)
+  opts = opts or {}
+
+  if vim.g.loaded_vimpector == nil then
+    error("Vimspector is not loaded")
+  end
+
+  local configurations = vim.api.nvim_call_function("vimspector#GetConfigurations", {})
+
+  if vim.tbl_isempty(configurations) then
+    return
+  end
+
+  pickers.new(opts, {
+    prompt_title = 'Vimspector Launch',
+    finder    = finders.new_table {
+      results = configurations
+    },
+    sorter = sorters.get_generic_fuzzy_sorter(),
+    attach_mappings = function(prompt_bufnr, map)
+      local start_debugger = function()
+        local selection = actions.get_selected_entry(prompt_bufnr)
+        actions.close(prompt_bufnr)
+
+        vim.cmd("call vimspector#LaunchWithSettings(#{ configuration: '" .. selection.value .. "'})")
+      end
+
+      map('i', '<CR>', start_debugger)
+      map('n', '<CR>', start_debugger)
+
+      return true
+    end
+  }):find()
+end
+
 return builtin
