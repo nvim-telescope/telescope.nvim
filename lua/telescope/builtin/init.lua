505441c2f00e67ae759a1395634bf4841bf06a74
Senghan Bright <senghan.bright@deltaprojects.com>
2020-11-23 10:45:39 +0100

    set default mapping.

--- lua/telescope/builtin/init.lua
+++ lua/telescope/builtin/init.lua
@@ -899,8 +899,8 @@ builtin.registers = function(opts)
     -- use levenshtein as n-gram doesn't support <2 char matches
     sorter = sorters.get_levenshtein_sorter(),
     attach_mappings = function(_, map)
+      map('i', '<CR>', actions.paste_register)
       map('i', '<C-e>', actions.edit_register)
-      map('i', '<C-p>', actions.paste_register)
 
       return true
     end,
