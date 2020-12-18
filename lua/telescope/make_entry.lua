5010dd62a705d1606a6dcdd592ac774f98a901de
Senghan Bright <senghan.bright@deltaprojects.com>
2020-12-18 15:40:35 +0100

    fix_spacing

--- lua/telescope/make_entry.lua
+++ lua/telescope/make_entry.lua
@@ -387,9 +387,9 @@ function make_entry.gen_from_buffer(opts)
     end
 
     return displayer {
-        {entry.bufnr, "Number"},
-        {entry.indicator, "Comment"},
-        display_bufname .. ":" .. entry.lnum,
+      {entry.bufnr, "Number"},
+      {entry.indicator, "Comment"},
+      display_bufname .. ":" .. entry.lnum
       }
   end
 
