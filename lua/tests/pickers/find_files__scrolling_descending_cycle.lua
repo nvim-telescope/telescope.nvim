080cef16e95a0d134c73d1198b994a9e7a728637
TJ DeVries <devries.timothyj@gmail.com>
2020-12-29 10:09:02 -0500

    fix test

--- lua/tests/pickers/find_files__scrolling_descending_cycle.lua
+++ lua/tests/pickers/find_files__scrolling_descending_cycle.lua
@@ -5,9 +5,6 @@ local tester = require('telescope.pickers._test')
 local helper = require('telescope.pickers._test_helpers')
 
 tester.builtin_picker('find_files', 'telescope<c-n>', {
-  post_typed = {
-    tester.not_ { 'plugin/telescope.vim', helper.get_selection_value },
-  },
   post_close = {
     tester.not_ { 'plugin/telescope.vim', helper.get_file },
   },
