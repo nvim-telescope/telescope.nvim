00e898a1f919a2330c3b0fdf21429627cbaf2175
Simon Hauser <Simon-Hauser@outlook.de>
2021-01-15 16:42:26 +0100

    fix: preview for unnamed buffers (#434)

--- lua/telescope/previewers/term_previewer.lua
+++ lua/telescope/previewers/term_previewer.lua
@@ -259,7 +259,7 @@ previewers.vimgrep = defaulter(function(opts)
       local height = vim.api.nvim_win_get_height(win_id)
 
       local p = from_entry.path(entry, true)
-      if p == nil or p == '' then return end
+      if p == nil or p == '' or p == '[No Name]' then return end
       local lnum = entry.lnum or 0
 
       local context = math.floor(height / 2)
