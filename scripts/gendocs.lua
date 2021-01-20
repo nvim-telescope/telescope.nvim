21da47d3867a1b6a4c62a9f8c003e17a6ad12d9f
Simon Hauser <Simon-Hauser@outlook.de>
2021-01-20 22:04:01 +0100

    Docgen CI job

--- scripts/gendocs.lua
+++ scripts/gendocs.lua
@@ -5,11 +5,8 @@ local docgen = require('docgen')
 local docs = {}
 
 docs.test = function()
-  local input_dir = "./lua/telescope/"
-  local input_files = vim.fn.globpath(input_dir, "**/*.lua", false, true)
-
   -- TODO: Fix the other files so that we can add them here.
-  input_files = {
+  local input_files = {
     "./lua/telescope/init.lua"
   }
 
@@ -17,14 +14,14 @@ docs.test = function()
     return #a < #b
   end)
 
-  local output_file = "./docs/telescope.txt"
+  local output_file = "./doc/telescope.txt"
   local output_file_handle = io.open(output_file, "w")
 
   for _, input_file in ipairs(input_files) do
     docgen.write(input_file, output_file_handle)
   end
 
-  output_file_handle:write(" vim:tw=78:ts=8:ft=help:norl:")
+  output_file_handle:write(" vim:tw=78:ts=8:ft=help:norl:\n")
   output_file_handle:close()
   vim.cmd [[checktime]]
 end
