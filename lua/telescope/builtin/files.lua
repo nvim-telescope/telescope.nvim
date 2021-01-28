98a6b46d02daa75fe4f9949f20d047aa5c19c848
TJ DeVries <devries.timothyj@gmail.com>
2021-01-28 15:50:53 -0500

    fix: lint

--- lua/telescope/builtin/files.lua
+++ lua/telescope/builtin/files.lua
@@ -1,4 +1,3 @@
-local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
 local action_set = require('telescope.actions.set')
 local finders = require('telescope.finders')
