ee5c8f076429380b06eff03480428c68c6393521
delphinus <me@delphinus.dev>
2021-01-31 22:15:17 +0900

    Do not call VimL func and use the libuv way

--- lua/telescope/builtin/internal.lua
+++ lua/telescope/builtin/internal.lua
@@ -406,7 +406,8 @@ internal.man_pages = function(opts)
   opts.sections = utils.get_default(opts.sections, {'1'})
   assert(vim.tbl_islist(opts.sections), 'sections should be a list')
   opts.man_cmd = utils.get_lazy_default(opts.man_cmd, function()
-    return vim.fn.has'mac' and {'apropos', ' '} or {'apropos', ''}
+    local is_darwin = vim.loop.os_uname().sysname == 'Darwin'
+    return is_darwin and {'apropos', ' '} or {'apropos', ''}
   end)
   opts.entry_maker = opts.entry_maker or make_entry.gen_from_apropos(opts)
 
