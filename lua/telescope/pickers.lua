779704bf266c38f71e080599798cc4437393ce50
Raphael <glepnir@gopherhub.org>
2021-02-03 12:23:28 +0800

    Add user autocmd TelescopeFindPre (#491)
    * Add user autocmd Telescope
    
    * change name to TelescopeFindPre
    
    * update Readme for Telescope user autocmds

--- lua/telescope/pickers.lua
+++ lua/telescope/pickers.lua
@@ -326,6 +326,9 @@ function Picker:find()
 
   self.original_win_id = a.nvim_get_current_win()
 
+  -- User autocmd run it before create Telescope window
+  vim.cmd'do User TelescopeFindPre'
+
   -- Create three windows:
   -- 1. Prompt window
   -- 2. Options window
