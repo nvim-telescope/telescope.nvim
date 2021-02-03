38b6ff713e438ba02b6f4c04930863bcb14a4011
Fabian David Schmidt <fabian.david.schmidt@hotmail.com>
2021-02-03 10:47:06 +0100

    Adding visual selection for grep_string

--- lua/telescope/utils.lua
+++ lua/telescope/utils.lua
@@ -284,4 +284,42 @@ function utils.strcharpart(str, nchar, charlen)
   return str:sub(nbyte + 1, nbyte + len)
 end
 
+function utils.get_visual_selection_range()
+    local line_v, column_v = unpack(vim.fn.getpos("v"), 2, 3)
+    local line_cur, column_cur = unpack(vim.fn.getcurpos(), 2, 3)
+    -- backwards visual selection
+    if line_v > line_cur or (line_v == line_cur and column_cur < column_v) then
+        return line_cur, column_cur, line_v, column_v
+    end
+    return line_v, column_v, line_cur, column_cur
+end
+
+function utils.get_visual_selection(delimiter)
+    local delimiter = delimiter or ' '
+    local mode = vim.fn.mode()
+    local line_start, column_start, line_end, column_end = utils.get_visual_selection_range()
+    local lines = vim.api.nvim_buf_get_lines(0, line_start - 1, line_end, false)
+
+    local concat = {}
+    local first_line = 1
+    local last_line = line_end - (line_start - 1)
+    if first_line == last_line then
+        -- get difference in columns, inclusive
+        column_end = column_end - column_start + 1
+    end
+    for row=first_line,last_line do
+        local line = lines[row]
+        if mode ~= 'V' then
+            if row == first_line then
+                line = line:sub(column_start)
+            end
+            if row == last_line then
+                line = line:sub(1, column_end)
+            end
+        end
+        table.insert(concat, vim.trim(line))
+    end
+    return table.concat(concat, delimiter)
+end
+
 return utils
