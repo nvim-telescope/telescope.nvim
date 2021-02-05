d74b5406aa41a265ff25a3510de2ecf113fafbac
TJ DeVries <devries.timothyj@gmail.com>
2021-02-04 21:43:46 -0500

    feat: special case attach_mappings to be overridable and defaultable

--- lua/telescope/pickers.lua
+++ lua/telescope/pickers.lua
@@ -22,25 +22,6 @@ local EntryManager = require('telescope.entry_manager')
 
 local get_default = utils.get_default
 
--- TODO: Make this work with deep extend I think.
-local extend = function(opts, defaults)
-  local result = {}
-
-  for k, v in pairs(opts or {}) do
-    assert(type(k) == 'string', "Should be string, opts")
-    result[k] = v
-  end
-
-  for k, v in pairs(defaults or {}) do
-    if result[k] == nil then
-      assert(type(k) == 'string', "Should be string, defaults")
-      result[k] = v
-    end
-  end
-
-  return result
-end
-
 local ns_telescope_matching = a.nvim_create_namespace('telescope_matching')
 local ns_telescope_prompt = a.nvim_create_namespace('telescope_prompt')
 local ns_telescope_prompt_prefix = a.nvim_create_namespace('telescope_prompt_prefix')
@@ -1020,7 +1001,31 @@ end
 
 
 pickers.new = function(opts, defaults)
-  return Picker:new(extend(opts, defaults))
+  local result = {}
+
+  for k, v in pairs(opts or {}) do
+    assert(type(k) == 'string', "Should be string, opts")
+    result[k] = v
+  end
+
+  for k, v in pairs(defaults or {}) do
+    if result[k] == nil then
+      assert(type(k) == 'string', "Should be string, defaults")
+      result[k] = v
+    else
+      -- For attach mappings, we want people to be able to pass in another function
+      -- and apply their mappings after we've applied our defaults.
+      if k == 'attach_mappings' then
+        local opt_value = result[k]
+        result[k] = function(...)
+          v(...)
+          return opt_value(...)
+        end
+      end
+    end
+  end
+
+  return Picker:new(result)
 end
 
 function pickers.on_close_prompt(prompt_bufnr)
