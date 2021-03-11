0eb5746a914f0f56f6f70eef3629ab7f235567b0
Chris Laidler <cjjlaidler@gmail.com>
2021-03-11 20:31:59 +0000

    fix: related to #13 telescope-project, enter insert mode on prompt close

--- lua/telescope/actions/init.lua
+++ lua/telescope/actions/init.lua
@@ -196,8 +196,8 @@ function actions.close_pum(_)
   end
 end
 
-local do_close = function(prompt_bufnr, keepinsert)
-  local picker = action_state.get_current_picker(prompt_bufnr)
+actions._close = function(prompt_bufnr, keepinsert)
+  local picker = actions.get_current_picker(prompt_bufnr)
   local prompt_win = state.get_status(prompt_bufnr).prompt_win
   local original_win_id = picker.original_win_id
 
@@ -217,7 +217,7 @@ local do_close = function(prompt_bufnr, keepinsert)
 end
 
 function actions.close(prompt_bufnr)
-  do_close(prompt_bufnr, false)
+  actions._close(prompt_bufnr, false)
 end
 
 actions.set_command_line = function(prompt_bufnr)
