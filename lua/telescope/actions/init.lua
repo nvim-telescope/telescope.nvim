a7a23c53ab927b01f59ec03fe74538f45f35ee89
Chris Laidler <cjjlaidler@gmail.com>
2021-03-11 20:39:29 +0000

    revert changes made in error

--- lua/telescope/actions/init.lua
+++ lua/telescope/actions/init.lua
@@ -59,7 +59,7 @@ function actions.get_current_line()
   return action_state.get_current_line()
 end
 
-function actions.get_current_picker(prompt_bufnr)
+function action_state.get_current_picker(prompt_bufnr)
   -- TODO(1.0): Remove
   action_is_deprecated("get_current_picker")
   return action_state.get_current_picker(prompt_bufnr)
