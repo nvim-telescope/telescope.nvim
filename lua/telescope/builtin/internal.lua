08f4d8dbffbcf120ceca41fefdd1e0362d86744f
Jesse Leite <jesseleite@gmail.com>
2021-03-14 15:09:32 -0400

    Not using `k`, use `_` to suppress linting errors?

--- lua/telescope/builtin/internal.lua
+++ lua/telescope/builtin/internal.lua
@@ -234,7 +234,7 @@ internal.oldfiles = function(opts)
   local results = {}
 
   if opts.include_current_session then
-    for k,buffer in ipairs(vim.split(vim.fn.execute(':buffers! t'), "\n")) do
+    for _, buffer in ipairs(vim.split(vim.fn.execute(':buffers! t'), "\n")) do
       local match = string.match(buffer, '%s*(%d+)')
       if match then
         local file = vim.api.nvim_buf_get_name(match)
@@ -245,7 +245,7 @@ internal.oldfiles = function(opts)
     end
   end
 
-  for k,file in ipairs(vim.v.oldfiles) do
+  for _, file in ipairs(vim.v.oldfiles) do
     if vim.loop.fs_lstat(file) and vim.fn.index(results, file) == -1 then
       table.insert(results, file)
     end
