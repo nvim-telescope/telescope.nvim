034b54d2245433c5ffd831463e580accb59b89d2
Brian Shu <littlebubu.shu@gmail.com>
2021-04-13 08:15:04 -0400

    started

--- lua/telescope/finders.lua
+++ lua/telescope/finders.lua
@@ -20,7 +20,6 @@ local _callable_obj = function()
   return obj
 end
 
-
 --[[ =============================================================
 
     JobFinder
@@ -107,6 +106,190 @@ function JobFinder:_find(prompt, process_result, process_complete)
 
   self.job:start()
 end
+
+local LiveFinder = _callable_obj()
+
+function LiveFinder:new(opts)
+  opts = opts or {}
+
+  assert(not opts.results, "`results` should be used with finder.new_table")
+  assert(not opts.static, "`static` should be used with finder.new_oneshot_job")
+
+  local obj = setmetatable({
+    fn = opts.fn,
+    entry_maker = opts.entry_maker or make_entry.from_string,
+    fn_command = opts.fn_command,
+    cwd = opts.cwd,
+    writer = opts.writer,
+
+    -- Maximum number of results to process.
+    --  Particularly useful for live updating large queries.
+    maximum_results = opts.maximum_results,
+  }, self)
+
+  return obj
+end
+
+function LiveFinder:_find(opts)
+end
+
+local OneshotJobFinder = _callable_obj()
+
+function OneshotJobFinder:new(opts)
+  opts = opts or {}
+
+  assert(not opts.results, "`results` should be used with finder.new_table")
+  assert(not opts.static, "`static` should be used with finder.new_oneshot_job")
+
+  local obj = setmetatable({
+    fn_command = opts.fn_command,
+    entry_maker = opts.entry_maker or make_entry.from_string,
+
+    cwd = opts.cwd,
+    writer = opts.writer,
+
+    maximum_results = opts.maximum_results,
+
+    _started = false,
+  }, self)
+
+  obj._find = coroutine.wrap(function(finder, _, process_result, process_complete)
+    local num_execution = 1
+    local num_results = 0
+
+    local results = setmetatable({}, {
+      __newindex = function(t, k, v)
+        rawset(t, k, v)
+        process_result(v)
+      end
+    })
+
+    local job_opts = finder:fn_command(_)
+    if not job_opts then
+      error(debug.traceback("expected `job_opts` from fn_command"))
+    end
+
+    local writer = nil
+    if job_opts.writer and Job.is_job(job_opts.writer) then
+      writer = job_opts.writer
+    elseif job_opts.writer then
+      writer = Job:new(job_opts.writer)
+    end
+
+    local on_output = function(_, line)
+      -- This will call the metamethod, process_result
+      num_results = num_results + 1
+      results[num_results] = finder.entry_maker(line)
+    end
+
+    local completed = false
+    local job = Job:new {
+      command = job_opts.command,
+      args = job_opts.args,
+      cwd = job_opts.cwd or finder.cwd,
+
+      maximum_results = finder.maximum_results,
+
+      writer = writer,
+
+      enable_recording = false,
+
+      on_stdout = on_output,
+      on_stderr = on_output,
+
+      on_exit = function()
+        process_complete()
+        completed = true
+      end,
+    }
+
+    job:start()
+
+    while true do
+      finder, _, process_result, process_complete = coroutine.yield()
+      num_execution = num_execution + 1
+
+      local current_count = num_results
+      for index = 1, current_count do
+        process_result(results[index])
+      end
+
+      if completed then
+        process_complete()
+      end
+    end
+  end)
+
+  return obj
+end
+
+function OneshotJobFinder:old_find(_, process_result, process_complete)
+  local first_run = false
+
+  if not self._started then
+    first_run = true
+
+    self._started = true
+
+  end
+
+  -- First time we get called, start on up that job.
+  -- Every time after that, just use the results LUL
+  if not first_run then
+    return
+  end
+end
+
+
+
+--[[ =============================================================
+Static Finders
+
+A static finder has results that never change.
+They are passed in directly as a result.
+-- ============================================================= ]]
+local StaticFinder = _callable_obj()
+
+function StaticFinder:new(opts)
+  assert(opts, "Options are required. See documentation for usage")
+
+  local input_results
+  if vim.tbl_islist(opts) then
+    input_results = opts
+  else
+    input_results = opts.results
+  end
+
+  local entry_maker = opts.entry_maker or make_entry.gen_from_string()
+
+  assert(input_results)
+  assert(input_results, "Results are required for static finder")
+  assert(type(input_results) == 'table', "self.results must be a table")
+
+  local results = {}
+  for k, v in ipairs(input_results) do
+    local entry = entry_maker(v)
+
+    if entry then
+      entry.index = k
+      table.insert(results, entry)
+    end
+  end
+
+  return setmetatable({ results = results }, self)
+end
+
+function StaticFinder:_find(_, process_result, process_complete)
+  for _, v in ipairs(self.results) do
+    process_result(v)
+  end
+
+  process_complete()
+end
+
+
+-- local
+
 
 --- Return a new Finder
 --
