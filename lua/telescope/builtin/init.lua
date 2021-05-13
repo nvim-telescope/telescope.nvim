2a41ff18da0d4772fef8efd082526b09fe4dea0a
Ben Smith <bsmithdev@mailbox.org>
2021-05-13 14:31:10 -0700

    break(git_files): change show_untracked default to false

--- lua/telescope/builtin/init.lua
+++ lua/telescope/builtin/init.lua
@@ -132,7 +132,7 @@ builtin.current_buffer_tags = require_on_exported_call("telescope.builtin.files"
 ---@param opts table: options to pass to the picker
 ---@field cwd string: specify the path of the repo
 ---@field use_git_root boolean: if we should use git root as cwd or the cwd (important for submodule) (default: true)
----@field show_untracked boolean: if true, adds `--others` flag to command and shows untracked files (default: true)
+---@field show_untracked boolean: if true, adds `--others` flag to command and shows untracked files (default: false)
 ---@field recurse_submodules boolean: if true, adds the `--recurse-submodules` flag to command (default: false)
 ---@field git_command table: command that will be exectued. {"git","ls-files","--exclude-standard","--cached"}
 builtin.git_files = require_on_exported_call("telescope.builtin.git").files
