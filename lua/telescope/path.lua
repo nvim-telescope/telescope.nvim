36a7b1cbd6fea21638826e22e810b0f015399b5b
Deniz <deniz@admentum.se>
2021-06-18 01:06:42 +0200

    remove question, it's decided

--- lua/telescope/path.lua
+++ lua/telescope/path.lua
@@ -64,7 +64,6 @@ path.smart = (function()
         end
       end
       if #dirs ~= 0 then
-        -- Q: Should we preserve the logic if the results are the same? 1 more maybe?
         if (max == 1 and #dirs >= 2) then
           max = #dirs - 2
         end
