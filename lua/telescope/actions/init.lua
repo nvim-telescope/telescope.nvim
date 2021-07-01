cd3d26b4f775a104d208c4b63ed67391a7fc5339
Fabian David Schmidt <fabian.david.schmidt@gmail.com>
2021-07-01 14:40:18 +0200

    fix: docgen & remove actions from actions/init.lua

--- lua/telescope/actions/init.lua
+++ lua/telescope/actions/init.lua
@@ -144,45 +144,6 @@ function actions.toggle_selection(prompt_bufnr)
   current_picker:toggle_selection(current_picker:get_selection_row())
 end
 
---- Apply `f` to entries of current picker and returns list of mapped entries
---- `f` takes (entry, index, row) as viable arguments in order
----@param prompt_bufnr number: The prompt bufnr
----@param f function: function to apply on entries of picker
----@return table: result from mapped entries
-function actions.map_entries(prompt_bufnr, f)
-  local current_picker = action_state.get_current_picker(prompt_bufnr)
-  local results = {}
-  local index = 1
-  local row, result
-    -- indices are 1-indexed, rows are 0-indexed
-  for entry in current_picker.manager:iter() do
-    row = current_picker:get_row(index)
-    -- assign result to variable as function w/o return value
-    -- results in error upon table.insert as it's not technically nil but 'empty'
-    result = f(entry, index, row)
-    results[index] = result
-    index = index + 1
-  end
-  return results
-end
-
---- Apply `f` to multi selections of current picker and returns list of mapped selections.
----@param prompt_bufnr number: The prompt bufnr
----@param f function: function to apply on multi selection of picker
----@return table: result from `f` applied to multi selections
-function actions.map_selections(prompt_bufnr, f)
-  local current_picker = action_state.get_current_picker(prompt_bufnr)
-  local results = {}
-  local result
-  for _, selection in ipairs(current_picker:get_multi_selection()) do
-    result = f(selection)
-    -- assign result to variable as function w/o return value
-    -- results in error upon table.insert as it's not technically nil but 'empty'
-    table.insert(results, result)
-  end
-  return results
-end
-
 --- Multi select all entries, possibly including entries not visible in results popup.
 ---@param prompt_bufnr number: The prompt bufnr
 function actions.select_all(prompt_bufnr)
