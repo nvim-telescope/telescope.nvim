99c7fa9023a095a1151dde1e12eeb7ac91f87399
caojoshua <cao.joshua@yahoo.com>
2021-07-10 12:49:10 -0700

    Shorten line that is too long.

--- lua/telescope/builtin/internal.lua
+++ lua/telescope/builtin/internal.lua
@@ -951,7 +951,8 @@ internal.jumplist = function(opts)
   -- reverse the list
   local sorted_jumplist = {}
   for i = #jumplist, 1, -1 do
-    jumplist[i].text = vim.api.nvim_buf_get_lines(jumplist[i].bufnr, jumplist[i].lnum, jumplist[i].lnum+1, false)[1] or ''
+    jumplist[i].text = vim.api.nvim_buf_get_lines(jumplist[i].bufnr, jumplist[i].lnum, jumplist[i].lnum+1,
+      false)[1] or ''
     table.insert(sorted_jumplist, jumplist[i])
   end
 
