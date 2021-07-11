ff2b49229149f40748dd4eff93bc5d766392ea4c
anott03 <amitav_nott@ryecountryday.org>
2021-07-11 16:09:47 -0400

    better deprecation notice

--- lua/telescope/path.lua
+++ lua/telescope/path.lua
@@ -1,26 +1,7 @@
-local path = {}
-
-path.separator = package.config:sub(1, 1)
-path.home = vim.fn.expand("~")
-
-path.make_relative = function()
-  error("telescope.path is deprecated. please use plenary.path instead")
-end
-
-path.shorten = function()
-  error("telescope.path is deprecated. please use plenary.path instead")
-end
-
-path.normalize = function()
-  error("telescope.path is deprecated. please use plenary.path instead")
-end
-
-path.read_file = function()
-  error("telescope.path is deprecated. please use plenary.path instead")
-end
-
-path.read_file_async = function()
-  error("telescope.path is deprecated. please use plenary.path instead")
-end
+local path = setmetatable({}, {
+  __index = function()
+    error("telescope.path is deprecated. please use plenary.path instead")
+  end
+})
 
 return path
