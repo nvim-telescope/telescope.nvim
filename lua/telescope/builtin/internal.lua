7852d0d486b492910fc1811198a8670ad67a9bcd
wongxy <xiyao.wong@foxmail.com>
2021-07-14 09:32:01 +0800

    chore: format code
    use spaces as indentation

--- lua/telescope/builtin/internal.lua
+++ lua/telescope/builtin/internal.lua
@@ -634,90 +634,90 @@ internal.buffers = function(opts)
 end
 
 internal.colorscheme = function(opts)
-	local before_color = vim.api.nvim_exec("colorscheme", true)
-	local need_restore = true
+  local before_color = vim.api.nvim_exec('colorscheme', true)
+  local need_restore = true
 
-	local colors = vim.list_extend(opts.colors or {}, vim.fn.getcompletion("", "color"))
+  local colors = vim.list_extend(opts.colors or {}, vim.fn.getcompletion('', 'color'))
 
-	local bufnr = vim.api.nvim_get_current_buf()
-	local p = vim.api.nvim_buf_get_name(bufnr)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local p = vim.api.nvim_buf_get_name(bufnr)
 
-	local previewer
-	if vim.fn.buflisted(bufnr) ~= 1 then
-		local deleted = false
-		local function del_win(win_id)
-			if win_id == nil or not vim.api.nvim_win_is_valid(win_id) then
-				return
-			end
+  local previewer
+  if vim.fn.buflisted(bufnr) ~= 1 then
+    local deleted = false
+    local function del_win(win_id)
+      if win_id == nil or not vim.api.nvim_win_is_valid(win_id) then
+        return
+      end
 
-			local bnr = vim.api.nvim_win_get_buf(win_id)
-			if vim.api.nvim_buf_is_valid(bnr) and not vim.api.nvim_buf_get_option(bnr, "buflisted") then
-				vim.cmd(string.format("silent! bdelete! %s", bnr))
-			end
+      local bnr = vim.api.nvim_win_get_buf(win_id)
+      if vim.api.nvim_buf_is_valid(bnr) and not vim.api.nvim_buf_get_option(bnr, 'buflisted') then
+        vim.cmd(string.format('silent! bdelete! %s', bnr))
+      end
 
-			if not vim.api.nvim_win_is_valid(win_id) then
-				return
-			end
-			pcall(vim.api.nvim_win_close, win_id, true)
-		end
+      if not vim.api.nvim_win_is_valid(win_id) then
+        return
+      end
+      pcall(vim.api.nvim_win_close, win_id, true)
+    end
 
-		previewer = previewers.new({
-			preview_fn = function(_, entry, status)
-				if not deleted then
-					deleted = true
-					del_win(status.preview_win)
-					del_win(status.preview_border_win)
-				end
-				vim.cmd("colorscheme " .. entry.value)
-			end,
-		})
-	else
-		previewer = previewers.new_buffer_previewer({
-			get_buffer_by_name = function()
-				return p
-			end,
-			define_preview = function(self, entry)
-				if vim.loop.fs_stat(p) then
-					conf.buffer_previewer_maker(p, self.state.bufnr, { bufname = self.state.bufname })
-				else
-					local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-					vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
-				end
-        vim.cmd("colorscheme " .. entry.value)
-			end,
-		})
-	end
+    previewer = previewers.new {
+      preview_fn = function(_, entry, status)
+        if not deleted then
+          deleted = true
+          del_win(status.preview_win)
+          del_win(status.preview_border_win)
+        end
+        vim.cmd('colorscheme ' .. entry.value)
+      end,
+    }
+  else
+    previewer = previewers.new_buffer_previewer {
+      get_buffer_by_name = function()
+        return p
+      end,
+      define_preview = function(self, entry)
+        if vim.loop.fs_stat(p) then
+          conf.buffer_previewer_maker(p, self.state.bufnr, { bufname = self.state.bufname })
+        else
+          local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+        end
+        vim.cmd('colorscheme ' .. entry.value)
+      end,
+    }
+  end
 
-	local picker = pickers.new(opts, {
-		prompt_title = "Change Colorscheme",
-		finder = finders.new_table({
-			results = colors,
-		}),
-		sorter = conf.generic_sorter(opts),
-		previewer = previewer,
-		attach_mappings = function(prompt_bufnr)
-			actions.select_default:replace(function()
-				local selection = action_state.get_selected_entry()
-				actions.close(prompt_bufnr)
+  local picker = pickers.new(opts, {
+    prompt_title = 'Change Colorscheme',
+    finder = finders.new_table {
+      results = colors,
+    },
+    sorter = conf.generic_sorter(opts),
+    previewer = previewer,
+    attach_mappings = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        local selection = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
 
-				need_restore = false
-				vim.cmd("colorscheme " .. selection.value)
-			end)
+        need_restore = false
+        vim.cmd('colorscheme ' .. selection.value)
+      end)
 
-			return true
-		end,
-	})
+      return true
+    end,
+  })
 
-	-- rewrite picker.close_windows. restore color if needed
-	local close_windows = picker.close_windows
-	picker.close_windows = function(status)
-		close_windows(status)
-		if need_restore then
-			vim.cmd("colorscheme " .. before_color)
-		end
-	end
+  -- rewrite picker.close_windows. restore color if needed
+  local close_windows = picker.close_windows
+  picker.close_windows = function(status)
+    close_windows(status)
+    if need_restore then
+      vim.cmd('colorscheme ' .. before_color)
+    end
+  end
 
-	picker:find()
+  picker:find()
 end
 
 internal.marks = function(opts)
