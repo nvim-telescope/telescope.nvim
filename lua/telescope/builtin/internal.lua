d23b9bba52d9a9d4dc8865f65ce62badeadeab21
wongxy <xiyao.wong@foxmail.com>
2021-07-15 17:36:13 +0800

    feat(colorscheme):  Add option to enable preview
    - set option `enable_preview` to true to enable preview of the theme
    - use util functions
    - put the current color first

--- lua/telescope/builtin/internal.lua
+++ lua/telescope/builtin/internal.lua
@@ -637,55 +637,64 @@ internal.colorscheme = function(opts)
   local before_color = vim.api.nvim_exec('colorscheme', true)
   local need_restore = true
 
-  local colors = vim.list_extend(opts.colors or {}, vim.fn.getcompletion('', 'color'))
+  local colors = opts.colors or { before_color }
+  if not vim.tbl_contains(colors, before_color) then
+    table.insert(colors, 1, before_color)
+  end
 
-  local bufnr = vim.api.nvim_get_current_buf()
-  local p = vim.api.nvim_buf_get_name(bufnr)
+  colors = vim.list_extend(
+    colors,
+    vim.tbl_filter(function(color)
+      return color ~= before_color
+    end, vim.fn.getcompletion(
+      '',
+      'color'
+    ))
+  )
 
   local previewer
-  if vim.fn.buflisted(bufnr) ~= 1 then
-    local deleted = false
-    local function del_win(win_id)
-      if win_id == nil or not vim.api.nvim_win_is_valid(win_id) then
-        return
+  if opts.enable_preview then
+    -- define previewer
+    local bufnr = vim.api.nvim_get_current_buf()
+    local p = vim.api.nvim_buf_get_name(bufnr)
+
+    -- don't need previewer
+    if vim.fn.buflisted(bufnr) ~= 1 then
+      local deleted = false
+      local function del_win(win_id)
+        if win_id and vim.api.nvim_win_is_valid(win_id) then
+          utils.buf_delete(vim.api.nvim_win_get_buf(win_id))
+          pcall(vim.api.nvim_win_close, win_id, true)
+        end
       end
 
-      local bnr = vim.api.nvim_win_get_buf(win_id)
-      if vim.api.nvim_buf_is_valid(bnr) and not vim.api.nvim_buf_get_option(bnr, 'buflisted') then
-        vim.cmd(string.format('silent! bdelete! %s', bnr))
-      end
-
-      if not vim.api.nvim_win_is_valid(win_id) then
-        return
-      end
-      pcall(vim.api.nvim_win_close, win_id, true)
+      previewer = previewers.new {
+        preview_fn = function(_, entry, status)
+          if not deleted then
+            deleted = true
+            del_win(status.preview_win)
+            del_win(status.preview_border_win)
+          end
+          vim.cmd('colorscheme ' .. entry.value)
+        end,
+      }
+    else
+      -- show current buffer content in previewer
+      previewer = previewers.new_buffer_previewer {
+        get_buffer_by_name = function()
+          return p
+        end,
+        define_preview = function(self, entry)
+          if vim.loop.fs_stat(p) then
+            conf.buffer_previewer_maker(p, self.state.bufnr, { bufname = self.state.bufname })
+          else
+            local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+            vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
+          end
+          vim.cmd('colorscheme ' .. entry.value)
+        end,
+      }
     end
-
-    previewer = previewers.new {
-      preview_fn = function(_, entry, status)
-        if not deleted then
-          deleted = true
-          del_win(status.preview_win)
-          del_win(status.preview_border_win)
-        end
-        vim.cmd('colorscheme ' .. entry.value)
-      end,
-    }
-  else
-    previewer = previewers.new_buffer_previewer {
-      get_buffer_by_name = function()
-        return p
-      end,
-      define_preview = function(self, entry)
-        if vim.loop.fs_stat(p) then
-          conf.buffer_previewer_maker(p, self.state.bufnr, { bufname = self.state.bufname })
-        else
-          local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, lines)
-        end
-        vim.cmd('colorscheme ' .. entry.value)
-      end,
-    }
   end
 
   local picker = pickers.new(opts, {
@@ -708,12 +717,14 @@ internal.colorscheme = function(opts)
     end,
   })
 
-  -- rewrite picker.close_windows. restore color if needed
-  local close_windows = picker.close_windows
-  picker.close_windows = function(status)
-    close_windows(status)
-    if need_restore then
-      vim.cmd('colorscheme ' .. before_color)
+  if opts.enable_preview then
+    -- rewrite picker.close_windows. restore color if needed
+    local close_windows = picker.close_windows
+    picker.close_windows = function(status)
+      close_windows(status)
+      if need_restore then
+        vim.cmd('colorscheme ' .. before_color)
+      end
     end
   end
 
