6e619f3a16f7ae95a4c73b559efd511f8678c210
Luke Kershaw <35707277+l-kershaw@users.noreply.github.com>
2021-07-20 18:07:40 +0100

    refactor: only calculate `filename` when needed

--- lua/telescope/builtin/files.lua
+++ lua/telescope/builtin/files.lua
@@ -297,6 +297,9 @@ files.file_browser = function(opts)
           if is_dir(t.value) then retpath = retpath .. os_sep end
           return retpath
         end
+        if k == "filename" then
+          return Path:new(t[1]):absolute()
+        end
 
         return rawget(t, rawget({ value = 1 }, k))
       end
@@ -304,7 +307,6 @@ files.file_browser = function(opts)
       return function(line)
         local tbl = {line}
         tbl.ordinal = Path:new(line):make_relative(opts.cwd)
-        tbl.filename = Path:new(line):absolute()
         return setmetatable(tbl, mt)
       end
     end
