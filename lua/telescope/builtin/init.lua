7363ed41537e19b84c2135ad0bd256c8ff51cf7e
Simon Hauser <Simon-Hauser@outlook.de>
2021-08-04 22:21:18 +0200

    docgen

--- lua/telescope/builtin/init.lua
+++ lua/telescope/builtin/init.lua
@@ -316,7 +316,7 @@ builtin.lsp_references = require("telescope.builtin.lsp").references
 
 --- Goto the definition of the word under the cursor, if there's only one, otherwise show all options in Telescope
 ---@param opts table: options to pass to the picker
----@field jump_type string: how to go to definition if there is only one, values: nil, "tab", "split", "vsplit", "never"
+---@field jump_type string: how to go to definition if there is only one, values: "tab", "split", "vsplit", "never"
 builtin.lsp_definitions = require("telescope.builtin.lsp").definitions
 
 --- Goto the implementation of the word under the cursor if there's only one, otherwise show all options in Telescope
