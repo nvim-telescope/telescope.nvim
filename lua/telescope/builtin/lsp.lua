06e55d3b3afe4b3070783699ecc8985f0236caa4
Marten Trip <me@marten.xyz>
2021-08-11 20:03:31 +0200

    fix: builtin `list_or_jump` now handles non list replies from the server (#1101)
    fix for servers such as zls and clojure_lsp

--- lua/telescope/builtin/lsp.lua
+++ lua/telescope/builtin/lsp.lua
@@ -58,6 +58,12 @@ local function list_or_jump(action, title, opts)
   local flattened_results = {}
   for _, server_results in pairs(result) do
     if server_results.result then
+      -- textDocument/definition can return Location or Location[]
+      if not vim.tbl_islist(server_results.result) then
+        flattened_results = { server_results.result }
+        break
+      end
+
       vim.list_extend(flattened_results, server_results.result)
     end
   end
