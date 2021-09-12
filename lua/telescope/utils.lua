60c5dd769e3318a5d0678dbff0dff8b2fbe9b4c9
Simon Hauser <Simon-Hauser@outlook.de>
2021-09-12 11:48:20 +0200

    stylua and integrate into path_display

--- lua/telescope/utils.lua
+++ lua/telescope/utils.lua
@@ -265,11 +265,11 @@ utils.path_smart = (function()
   local paths = {}
   return function(filepath)
     local final = filepath
-    if (#paths ~= 0) then
+    if #paths ~= 0 then
       local dirs = vim.split(filepath, "/")
       local max = 1
       for _, p in pairs(paths) do
-        if (#p > 0 and p ~= filepath) then
+        if #p > 0 and p ~= filepath then
           local _dirs = vim.split(p, "/")
           for i = 1, math.min(#dirs, #_dirs) do
             if (dirs[i] ~= _dirs[i]) and i > max then
@@ -280,22 +280,22 @@ utils.path_smart = (function()
         end
       end
       if #dirs ~= 0 then
-        if (max == 1 and #dirs >= 2) then
+        if max == 1 and #dirs >= 2 then
           max = #dirs - 2
         end
-        final = ''
+        final = ""
         for k, v in pairs(dirs) do
           if k >= max - 1 then
-            final = final .. (#final > 0 and '/' or '') .. v
+            final = final .. (#final > 0 and "/" or "") .. v
           end
         end
       end
     end
     if not paths[filepath] then
-      paths[filepath] = ''
+      paths[filepath] = ""
       table.insert(paths, filepath)
     end
-    if (final and final ~= filepath) then
+    if final and final ~= filepath then
       return "../" .. final
     else
       return filepath
@@ -342,6 +342,8 @@ utils.transform_path = function(opts, path)
   elseif type(path_display) == "table" then
     if vim.tbl_contains(path_display, "tail") or path_display.tail then
       transformed_path = utils.path_tail(transformed_path)
+    elseif vim.tbl_contains(path_display, "smart") or path_display.smart then
+      transformed_path = utils.path_smart(transformed_path)
     else
       if not vim.tbl_contains(path_display, "absolute") or path_display.absolute == false then
         local cwd
