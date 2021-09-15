fea108d20b92d671a5deb810ee93df78db37fbf3
Anthony Nichols <nichols.anthony@hotmail.com>
2021-09-15 16:32:49 -0500

    fix: added @field pattern annotation to git_branches

--- lua/telescope/builtin/init.lua
+++ lua/telescope/builtin/init.lua
@@ -182,6 +182,7 @@ builtin.git_bcommits = require_on_exported_call("telescope.builtin.git").bcommit
 ---@param opts table: options to pass to the picker
 ---@field cwd string: specify the path of the repo
 ---@field use_git_root boolean: if we should use git root as cwd or the cwd (important for submodule) (default: true)
+---@field pattern string: specify the pattern to match all refs
 builtin.git_branches = require_on_exported_call("telescope.builtin.git").branches
 
 --- Lists git status for current directory
