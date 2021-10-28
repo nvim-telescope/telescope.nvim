nvim-telescope/telescope.nvim

commit 6f829bf6bc333809dd97e532dbc88a7fcf297b02

Use plenary.path over vim.split '\n'

lua/telescope/builtin/files.lua
@@ -569,7 +569,7 @@ files.tags = function(opts)
     local data = assert(vim.loop.fs_read(fd, stat.size, 0))
     assert(vim.loop.fs_close(fd))
 
-    for _, line in ipairs(vim.split(data, "\n")) do
+    for line in Path:new(vim.fn.expand(ctags_file)):iter() do
       results[#results + 1] = line
     end
   end

commit a7ede4f5ee21404ff91b45f18a5e19d5b4d3dcdf

fix: buitin.tags broke when tags-file was in wildignore
regression from
6f829bf6bc333809dd97e532dbc88a7fcf297b02
https://github.com/nvim-telescope/telescope.nvim/pull/1122

lua/telescope/builtin/files.lua
@@ -566,7 +566,7 @@ files.tags = function(opts)
 
   local results = {}
   for _, ctags_file in ipairs(tagfiles) do
-    for line in Path:new(vim.fn.expand(ctags_file)):iter() do
+    for line in Path:new(vim.fn.expand(ctags_file, true)):iter() do
       results[#results + 1] = line
     end
   end