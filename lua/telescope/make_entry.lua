28c909eabaaf38502c486f50d908a83cfa67f1b3
Luke Kershaw <35707277+l-kershaw@users.noreply.github.com>
2021-11-25 22:50:16 +0000

    fix: check for Windows drive letter when parsing vimgrep

--- lua/telescope/make_entry.lua
+++ lua/telescope/make_entry.lua
@@ -112,7 +112,12 @@ do
 
   -- Gets called only once to parse everything out for the vimgrep, after that looks up directly.
   local parse = function(t)
-    local _, _, filename, lnum, col, text = string.find(t.value, [[([^:]+):(%d+):(%d+):(.*)]])
+    local start, _, filename, lnum, col, text = string.find(t.value, [[([^:]+):(%d+):(%d+):(.*)]])
+
+    -- Handle Windows drive letter (e.g. "C:") at the beginning (if present)
+    if Path.path_sep == "\\" and start == 3 then
+      filename = string.sub(t.value, 1, 3) .. filename
+    end
 
     local ok
     ok, lnum = pcall(tonumber, lnum)
