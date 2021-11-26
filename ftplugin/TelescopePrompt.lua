b415e862bf248d66c40e242de4a0c76c68e278f1
Luke Kershaw <35707277+l-kershaw@users.noreply.github.com>
2021-11-26 21:43:51 +0000

    fix: set `nowrap` in prompt buffer (#1499)
    - fixes `strdisplaywidth` calculations for `dynamic_preview_title`

--- ftplugin/TelescopePrompt.lua
+++ ftplugin/TelescopePrompt.lua
@@ -2,6 +2,9 @@
 vim.opt_local.formatoptions:remove "t"
 vim.opt_local.formatoptions:remove "c"
 
+-- Don't include `showbreak` when calculating strdisplaywidth
+vim.opt_local.wrap = false
+
 -- There's also no reason to enable textwidth here anyway
 vim.opt_local.textwidth = 0
 vim.opt_local.scrollbind = false
