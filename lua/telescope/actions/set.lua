1a91238a6ad7a1f77fabdbee7225692a04b20d48
fdschmidt93 <39233597+fdschmidt93@users.noreply.github.com>
2022-05-20 12:08:33 +0200

    fix: wrong win on selection (#1951)

--- lua/telescope/actions/set.lua
+++ lua/telescope/actions/set.lua
@@ -132,6 +132,7 @@ action_set.edit = function(prompt_bufnr, command)
 
   local picker = action_state.get_current_picker(prompt_bufnr)
   require("telescope.pickers").on_close_prompt(prompt_bufnr)
+  pcall(vim.api.nvim_set_current_win, picker.original_win_id)
   local win_id = picker.get_selection_window(picker, entry)
 
   if picker.push_cursor_on_edit then
