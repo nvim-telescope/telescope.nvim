074d5eacc44d232ce767621017b43d278c42924c
Bjarki Baldursson Harksen <bjarki31@gmail.com>
2022-06-09 01:36:13 +0000

    feat: add option `use_default_opts` to `builtin.builtin`

--- lua/telescope/builtin/internal.lua
+++ lua/telescope/builtin/internal.lua
@@ -33,6 +33,7 @@ local internal = {}
 
 internal.builtin = function(opts)
   opts.include_extensions = utils.get_default(opts.include_extensions, false)
+  opts.use_default_opts = utils.get_default(opts.use_default_opts, false)
 
   local objs = {}
 
@@ -91,15 +92,20 @@ internal.builtin = function(opts)
         -- we do this to avoid any surprises
         opts.include_extensions = nil
 
+        local picker_opts
+        if not opts.use_default_opts then
+          picker_opts = opts
+        end
+
         if string.match(selection.text, " : ") then
           -- Call appropriate function from extensions
           local split_string = vim.split(selection.text, " : ")
           local ext = split_string[1]
           local func = split_string[2]
-          require("telescope").extensions[ext][func](opts)
+          require("telescope").extensions[ext][func](picker_opts)
         else
           -- Call appropriate telescope builtin
-          require("telescope.builtin")[selection.text](opts)
+          require("telescope.builtin")[selection.text](picker_opts)
         end
       end)
       return true
