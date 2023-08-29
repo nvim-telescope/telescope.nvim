1e6c4438cc45ce4edd7e467cb317136ebb76f53d
James Trew <j.trew10@gmail.com>
2023-08-28 23:25:58 -0400

    feat(actions): add quickfix list autocmd event exec
    Add `QuickFixCmdPre` and `QuickFixCmdPost` event triggers to
    `send_selected_to_qf` and `send_all_to_qf`

--- lua/telescope/actions/init.lua
+++ lua/telescope/actions/init.lua
@@ -855,6 +855,7 @@ local send_selected_to_qf = function(prompt_bufnr, mode, target)
   local prompt = picker:_get_prompt()
   actions.close(prompt_bufnr)
 
+  vim.api.nvim_exec_autocmds("QuickFixCmdPre", {})
   if target == "loclist" then
     vim.fn.setloclist(picker.original_win_id, qf_entries, mode)
   else
@@ -862,6 +863,7 @@ local send_selected_to_qf = function(prompt_bufnr, mode, target)
     vim.fn.setqflist(qf_entries, mode)
     vim.fn.setqflist({}, "a", { title = qf_title })
   end
+  vim.api.nvim_exec_autocmds("QuickFixCmdPost", {})
 end
 
 local send_all_to_qf = function(prompt_bufnr, mode, target)
@@ -876,6 +878,7 @@ local send_all_to_qf = function(prompt_bufnr, mode, target)
   local prompt = picker:_get_prompt()
   actions.close(prompt_bufnr)
 
+  vim.api.nvim_exec_autocmds("QuickFixCmdPre", {})
   if target == "loclist" then
     vim.fn.setloclist(picker.original_win_id, qf_entries, mode)
   else
@@ -883,6 +886,7 @@ local send_all_to_qf = function(prompt_bufnr, mode, target)
     local qf_title = string.format([[%s (%s)]], picker.prompt_title, prompt)
     vim.fn.setqflist({}, "a", { title = qf_title })
   end
+  vim.api.nvim_exec_autocmds("QuickFixCmdPost", {})
 end
 
 --- Sends the selected entries to the quickfix list, replacing the previous entries.
