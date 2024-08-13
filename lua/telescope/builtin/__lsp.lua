34edb9b364f50b3754e4b37aae874056aa9d7c54
James Trew <j.trew10@gmail.com>
2024-08-12 21:03:33 -0400

    remove unused import

--- lua/telescope/builtin/__lsp.lua
+++ lua/telescope/builtin/__lsp.lua
@@ -5,7 +5,6 @@ local lsp = vim.lsp
 
 local channel = require("plenary.async.control").channel
 local actions = require "telescope.actions"
-local sorters = require "telescope.sorters"
 local conf = require("telescope.config").values
 local finders = require "telescope.finders"
 local make_entry = require "telescope.make_entry"
