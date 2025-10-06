2337a7d40a7ea0e49f30e41a37876cd1dbeaf2b9
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-10-06 02:31:28 -0600

    fix(lsp): annotations fixed, improved params caller
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/telescope/builtin/__lsp.lua
+++ lua/telescope/builtin/__lsp.lua
@@ -68,12 +68,12 @@ local function pick_call_hierarchy_item(call_hierarchy_items)
   return call_hierarchy_items[choice]
 end
 
----@param win number? Window handler
----@param extra table? Extra fields in params
----@return table|(fun(client: vim.lsp.Client): table) parmas to send to the server
+---@param win number|nil: Window handler
+---@param extra lsp.TextDocumentPositionParams|nil: Extra fields in params
+---@return lsp.TextDocumentPositionParams|(fun(client: vim.lsp.Client): lsp.TextDocumentPositionParams): Params to send to the server
 local function client_position_params(win, extra)
   win = win or vim.api.nvim_get_current_win()
-  if vim.fn.has "nvim-0.11" == 0 then
+  if 1 ~= vim.fn.has "nvim-0.11" then
     local params = vim.lsp.util.make_position_params(win)
     if extra then
       params = vim.tbl_extend("force", params, extra)
