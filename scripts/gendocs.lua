5dbca7c251edab673cf0439bdb045fb28dc9c1de
2400060033 <botchakarunakar@gmail.com>
2025-11-28 00:11:03 +0530

    chore(docs): generate telescope documentation using docgen
    This PR adds a Lua script to generate documentation for Telescope plugin files.
    
    - Uses docgen to read all specified Telescope Lua source files.
    - Writes output to ./doc/telescope.txt with vim help formatting.
    - Ensures file is safely opened with assert.
    - Triggers Vim's checktime to reload changed docs.
    
    Improves maintainability and keeps plugin documentation up-to-date.

--- scripts/gendocs.lua
+++ scripts/gendocs.lua
@@ -2,6 +2,7 @@
 if RELOAD then
   RELOAD "telescope"
 end
+
 require("telescope").setup()
 
 local docgen = require "docgen"
@@ -9,7 +10,7 @@ local docgen = require "docgen"
 local docs = {}
 
 docs.test = function()
-  -- TODO: Fix the other files so that we can add them here.
+  -- TODO: Add other files once fixed
   local input_files = {
     "./lua/telescope/init.lua",
     "./lua/telescope/command.lua",
@@ -33,13 +34,13 @@ docs.test = function()
   }
 
   local output_file = "./doc/telescope.txt"
-  local output_file_handle = io.open(output_file, "w")
+  local output_file_handle = assert(io.open(output_file, "w"))
 
   for _, input_file in ipairs(input_files) do
     docgen.write(input_file, output_file_handle)
   end
 
-  output_file_handle:write " vim:tw=78:ts=8:ft=help:norl:\n"
+  output_file_handle:write(" vim:tw=78:ts=8:ft=help:norl:\n")
   output_file_handle:close()
   vim.cmd [[checktime]]
 end
