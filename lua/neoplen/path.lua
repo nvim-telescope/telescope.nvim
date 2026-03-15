9ff45fa4bde89a86a39dd25bfdb202b32e99c7e6
Christian Clason <c.clason@uni-graz.at>
2026-03-15 14:06:33 +0100

    feat(plenary): remove unused path functions

--- lua/neoplen/path.lua
+++ lua/neoplen/path.lua
@@ -15,6 +15,7 @@ local S_IF = {
 }
 
 local path = {}
+
 path.home = uv.os_homedir()
 
 path.sep = (function()
@@ -44,14 +45,6 @@ path.root = (function()
 end)()
 
 path.S_IF = S_IF
-
-local band = function(reg, value)
-  return bit.band(reg, value) == reg
-end
-
-local concat_paths = function(...)
-  return table.concat({ ... }, path.sep)
-end
 
 local function is_root(pathname)
   if path.sep == "\\" then
@@ -192,23 +185,8 @@ Path.__index = function(t, k)
   end
 end
 
--- TODO: Could use this to not have to call new... not sure
--- Path.__call = Path:new
-
-Path.__div = function(self, other)
-  assert(Path.is_path(self))
-  assert(Path.is_path(other) or type(other) == "string")
-
-  return self:joinpath(other)
-end
-
 Path.__tostring = function(self)
   return clean(self.filename)
-end
-
--- TODO: See where we concat the table, and maybe we could make this work.
-Path.__concat = function(self, other)
-  return self.filename .. other
 end
 
 Path.is_path = function(a)
@@ -290,14 +268,6 @@ function Path:_stat()
   -- return self._stat_result
 end
 
-function Path:_st_mode()
-  return self:_stat().mode or 0
-end
-
-function Path:joinpath(...)
-  return Path:new(self.filename, ...)
-end
-
 function Path:absolute()
   if self:is_absolute() then
     return _normalize_path(self.filename, self._cwd)
@@ -308,34 +278,6 @@ end
 
 function Path:exists()
   return not vim.tbl_isempty(self:_stat())
-end
-
-function Path:expand()
-  if is_uri(self.filename) then
-    return self.filename
-  end
-
-  -- TODO support windows
-  local expanded
-  if string.find(self.filename, "~") then
-    expanded = string.gsub(self.filename, "^~", uv.os_homedir())
-  elseif string.find(self.filename, "^%.") then
-    expanded = uv.fs_realpath(self.filename)
-    if expanded == nil then
-      expanded = vim.fn.fnamemodify(self.filename, ":p")
-    end
-  elseif string.find(self.filename, "%$") then
-    local rep = string.match(self.filename, "([^%$][^/]*)")
-    local val = os.getenv(rep)
-    if val then
-      expanded = string.gsub(string.gsub(self.filename, rep, val), "%$", "")
-    else
-      expanded = nil
-    end
-  else
-    expanded = self.filename
-  end
-  return expanded and expanded or error "Path not valid"
 end
 
 function Path:make_relative(cwd)
@@ -429,43 +371,60 @@ local function shorten_len(filename, len, exclude)
   return table.concat(final_path_components)
 end
 
-local shorten = (function()
-  local fallback = function(filename)
-    return shorten_len(filename, 1)
-  end
-
-  if jit and path.sep ~= "\\" then
-    local ffi = require "ffi"
-    ffi.cdef [[
-    typedef unsigned char char_u;
-    void shorten_dir(char_u *str);
-    ]]
-    local ffi_func = function(filename)
-      if not filename or is_uri(filename) then
-        return filename
-      end
-
-      local c_str = ffi.new("char[?]", #filename + 1)
-      ffi.copy(c_str, filename)
-      ffi.C.shorten_dir(c_str)
-      return ffi.string(c_str)
-    end
-    local ok = pcall(ffi_func, "/tmp/path/file.lua")
-    if ok then
-      return ffi_func
-    else
-      return fallback
-    end
-  end
-  return fallback
-end)()
-
 function Path:shorten(len, exclude)
   assert(len ~= 0, "len must be at least 1")
   if (len and len > 1) or exclude ~= nil then
     return shorten_len(self.filename, len, exclude)
   end
-  return shorten(self.filename)
+  return shorten_len(self.filename, 1)
+end
+
+function Path:is_absolute()
+  return is_absolute(self.filename, self._sep)
+end
+-- }}}
+
+function Path:_split()
+  return vim.split(self:absolute(), self._sep)
+end
+
+local _get_parent = (function()
+  local formatted = string.format("^(.+)%s[^%s]+", path.sep, path.sep)
+  return function(abs_path)
+    local parent = abs_path:match(formatted)
+    if parent ~= nil and not parent:find(path.sep) then
+      return parent .. path.sep
+    end
+    return parent
+  end
+end)()
+
+function Path:parent()
+  return Path:new(_get_parent(self:absolute()) or path.root(self:absolute()))
+end
+
+function Path:is_file()
+  return self:_stat().type == "file" and true or nil
+end
+
+-- TODO: Asyncify this and use vim.wait in the meantime.
+--  This will allow other events to happen while we're waiting!
+function Path:_read()
+  self = check_self(self)
+
+  local fd = assert(uv.fs_open(self:_fs_filename(), "r", 438)) -- for some reason test won't pass with absolute
+  local stat = assert(uv.fs_fstat(fd))
+  local data = assert(uv.fs_read(fd, stat.size, 0))
+  assert(uv.fs_close(fd))
+
+  return data
+end
+
+local band = function(reg, value)
+  return bit.band(reg, value) == reg
+end
+local concat_paths = function(...)
+  return table.concat({ ... }, path.sep)
 end
 
 function Path:mkdir(opts)
@@ -514,113 +473,6 @@ function Path:mkdir(opts)
   return true
 end
 
-function Path:rmdir()
-  if not self:exists() then
-    return
-  end
-
-  uv.fs_rmdir(self:absolute())
-end
-
-function Path:rename(opts)
-  opts = opts or {}
-  if not opts.new_name or opts.new_name == "" then
-    error "Please provide the new name!"
-  end
-
-  -- handles `.`, `..`, `./`, and `../`
-  if opts.new_name:match "^%.%.?/?\\?.+" then
-    opts.new_name = {
-      uv.fs_realpath(opts.new_name:sub(1, 3)),
-      opts.new_name:sub(4, #opts.new_name),
-    }
-  end
-
-  local new_path = Path:new(opts.new_name)
-
-  if new_path:exists() then
-    error "File or directory already exists!"
-  end
-
-  local status = uv.fs_rename(self:absolute(), new_path:absolute())
-  self.filename = new_path.filename
-
-  return status
-end
-
---- Copy files or folders with defaults akin to GNU's `cp`.
----@param opts table: options to pass to toggling registered actions
----@field destination string|Path: target file path to copy to
----@field recursive bool: whether to copy folders recursively (default: false)
----@field override bool: whether to override files (default: true)
----@field interactive bool: confirm if copy would override; precedes `override` (default: false)
----@field respect_gitignore bool: skip folders ignored by all detected `gitignore`s (default: false)
----@field hidden bool: whether to add hidden files in recursively copying folders (default: true)
----@field parents bool: whether to create possibly non-existing parent dirs of `opts.destination` (default: false)
----@field exists_ok bool: whether ok if `opts.destination` exists, if so folders are merged (default: true)
----@return table {[Path of destination]: bool} indicating success of copy; nested tables constitute sub dirs
-function Path:copy(opts)
-  opts = opts or {}
-  opts.recursive = F.if_nil(opts.recursive, false, opts.recursive)
-  opts.override = F.if_nil(opts.override, true, opts.override)
-
-  local dest = opts.destination
-  -- handles `.`, `..`, `./`, and `../`
-  if not Path.is_path(dest) then
-    if type(dest) == "string" and dest:match "^%.%.?/?\\?.+" then
-      dest = {
-        uv.fs_realpath(dest:sub(1, 3)),
-        dest:sub(4, #dest),
-      }
-    end
-    dest = Path:new(dest)
-  end
-  -- success is true in case file is copied, false otherwise
-  local success = {}
-  if not self:is_dir() then
-    if opts.interactive and dest:exists() then
-      vim.ui.select(
-        { "Yes", "No" },
-        { prompt = string.format("Overwrite existing %s?", dest:absolute()) },
-        function(_, idx)
-          success[dest] = uv.fs_copyfile(self:absolute(), dest:absolute(), { excl = idx ~= 1 }) or false
-        end
-      )
-    else
-      -- nil: not overriden if `override = false`
-      success[dest] = uv.fs_copyfile(self:absolute(), dest:absolute(), { excl = not opts.override }) or false
-    end
-    return success
-  end
-  -- dir
-  if opts.recursive then
-    dest:mkdir {
-      parents = F.if_nil(opts.parents, false, opts.parents),
-      exists_ok = F.if_nil(opts.exists_ok, true, opts.exists_ok),
-    }
-    local scan = require "neoplen.scandir"
-    local data = scan.scan_dir(self.filename, {
-      respect_gitignore = F.if_nil(opts.respect_gitignore, false, opts.respect_gitignore),
-      hidden = F.if_nil(opts.hidden, true, opts.hidden),
-      depth = 1,
-      add_dirs = true,
-    })
-    for _, entry in ipairs(data) do
-      local entry_path = Path:new(entry)
-      local suffix = table.remove(entry_path:_split())
-      local new_dest = dest:joinpath(suffix)
-      -- clear destination as it might be Path table otherwise failing w/ extend
-      opts.destination = nil
-      local new_opts = vim.tbl_deep_extend("force", opts, { destination = new_dest })
-      -- nil: not overriden if `override = false`
-      success[new_dest] = entry_path:copy(new_opts) or false
-    end
-    return success
-  else
-    error(string.format("Warning: %s was not copied as `recursive=false`", self:absolute()))
-  end
-end
-
 function Path:touch(opts)
   opts = opts or {}
 
@@ -644,111 +496,6 @@ function Path:touch(opts)
   uv.fs_close(fd)
 
   return true
-end
-
-function Path:rm(opts)
-  opts = opts or {}
-
-  local recursive = F.if_nil(opts.recursive, false, opts.recursive)
-  if recursive then
-    local scan = require "neoplen.scandir"
-    local abs = self:absolute()
-
-    -- first unlink all files
-    scan.scan_dir(abs, {
-      hidden = true,
-      on_insert = function(file)
-        uv.fs_unlink(file)
-      end,
-    })
-
-    local dirs = scan.scan_dir(abs, { add_dirs = true, hidden = true })
-    -- iterate backwards to clean up remaining dirs
-    for i = #dirs, 1, -1 do
-      uv.fs_rmdir(dirs[i])
-    end
-
-    -- now only abs is missing
-    uv.fs_rmdir(abs)
-  else
-    uv.fs_unlink(self:absolute())
-  end
-end
-
--- Path:is_* {{{
-function Path:is_dir()
-  -- TODO: I wonder when this would be better, if ever.
-  -- return self:_stat().type == 'directory'
-
-  return band(S_IF.DIR, self:_st_mode())
-end
-
-function Path:is_absolute()
-  return is_absolute(self.filename, self._sep)
-end
--- }}}
-
-function Path:_split()
-  return vim.split(self:absolute(), self._sep)
-end
-
-local _get_parent = (function()
-  local formatted = string.format("^(.+)%s[^%s]+", path.sep, path.sep)
-  return function(abs_path)
-    local parent = abs_path:match(formatted)
-    if parent ~= nil and not parent:find(path.sep) then
-      return parent .. path.sep
-    end
-    return parent
-  end
-end)()
-
-function Path:parent()
-  return Path:new(_get_parent(self:absolute()) or path.root(self:absolute()))
-end
-
-function Path:parents()
-  local results = {}
-  local cur = self:absolute()
-  repeat
-    cur = _get_parent(cur)
-    table.insert(results, cur)
-  until not cur
-  table.insert(results, path.root(self:absolute()))
-  return results
-end
-
-function Path:is_file()
-  return self:_stat().type == "file" and true or nil
-end
-
--- TODO:
---  Maybe I can use libuv for this?
-function Path:open() end
-
-function Path:close() end
-
-function Path:write(txt, flag, mode)
-  assert(flag, [[Path:write_text requires a flag! For example: 'w' or 'a']])
-
-  mode = mode or 438
-
-  local fd = assert(uv.fs_open(self:_fs_filename(), flag, mode))
-  assert(uv.fs_write(fd, txt, -1))
-  assert(uv.fs_close(fd))
-end
-
--- TODO: Asyncify this and use vim.wait in the meantime.
---  This will allow other events to happen while we're waiting!
-function Path:_read()
-  self = check_self(self)
-
-  local fd = assert(uv.fs_open(self:_fs_filename(), "r", 438)) -- for some reason test won't pass with absolute
-  local stat = assert(uv.fs_fstat(fd))
-  local data = assert(uv.fs_read(fd, stat.size, 0))
-  assert(uv.fs_close(fd))
-
-  return data
 end
 
 function Path:_read_async(callback)
@@ -823,51 +570,6 @@ function Path:head(lines)
   return data
 end
 
-function Path:tail(lines)
-  lines = lines or 10
-  self = check_self(self)
-  local chunk_size = 256
-
-  local fd = uv.fs_open(self:_fs_filename(), "r", 438)
-  if not fd then
-    return
-  end
-  local stat = assert(uv.fs_fstat(fd))
-  if stat.type ~= "file" then
-    uv.fs_close(fd)
-    return nil
-  end
-
-  local data = ""
-  local index, count = stat.size - 1, 0
-  while count < lines and index > 0 do
-    local real_index = index - chunk_size
-    if real_index < 0 then
-      chunk_size = chunk_size + real_index
-      real_index = 0
-    end
-
-    local read_chunk = assert(uv.fs_read(fd, chunk_size, real_index))
-
-    local i = #read_chunk
-    while i > 0 do
-      local char = read_chunk:sub(i, i)
-      if char == "\n" then
-        count = count + 1
-        if count >= lines then
-          break
-        end
-      end
-      index = index - 1
-      i = i - 1
-    end
-    data = read_chunk:sub(i + 1, #read_chunk) .. data
-  end
-  assert(uv.fs_close(fd))
-
-  return data
-end
-
 function Path:readlines()
   self = check_self(self)
 
@@ -924,23 +626,6 @@ function Path:readbyterange(offset, length)
   assert(uv.fs_close(fd))
 
   return data
-end
-
-function Path:find_upwards(filename)
-  local folder = Path:new(self)
-  local root = path.root(folder:absolute())
-
-  while true do
-    local p = folder:joinpath(filename)
-    if p:exists() then
-      return p
-    end
-    if folder:absolute() == root then
-      break
-    end
-    folder = folder:parent()
-  end
-  return nil
 end
 
 return Path
