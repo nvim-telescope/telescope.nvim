ca0cd0ba142859c88690025157524e148c6ce3f4
Christian Clason <c.clason@uni-graz.at>
2026-03-15 13:21:14 +0100

    feat(plenary): prefer builtin string functions

--- lua/neoplen/strings.lua
+++ lua/neoplen/strings.lua
@@ -1,112 +1,6 @@
-local path = require("neoplen.path").path
-
 local M = {}
 
-M.strdisplaywidth = (function()
-  local fallback = function(str, col)
-    str = tostring(str)
-    if vim.in_fast_event() then
-      return #str - (col or 0)
-    end
-    return vim.fn.strdisplaywidth(str, col)
-  end
-
-  if jit and path.sep ~= [[\]] then
-    local ffi = require "ffi"
-    ffi.cdef [[
-      typedef unsigned char char_u;
-      int linetabsize_col(int startcol, char_u *s);
-    ]]
-
-    local ffi_func = function(str, col)
-      str = tostring(str)
-      local startcol = col or 0
-      local s = ffi.new("char[?]", #str + 1)
-      ffi.copy(s, str)
-      return ffi.C.linetabsize_col(startcol, s) - startcol
-    end
-
-    local ok = pcall(ffi_func, "hello")
-    if ok then
-      return ffi_func
-    else
-      return fallback
-    end
-  else
-    return fallback
-  end
-end)()
-
-M.strcharpart = (function()
-  local fallback = function(str, nchar, charlen)
-    if vim.in_fast_event() then
-      return str:sub(nchar + 1, charlen)
-    end
-    return vim.fn.strcharpart(str, nchar, charlen)
-  end
-
-  if jit and path.sep ~= [[\]] then
-    local ffi = require "ffi"
-    ffi.cdef [[
-      typedef unsigned char char_u;
-      int utf_ptr2len(const char_u *const p);
-    ]]
-
-    local function utf_ptr2len(str)
-      local c_str = ffi.new("char[?]", #str + 1)
-      ffi.copy(c_str, str)
-      return ffi.C.utf_ptr2len(c_str)
-    end
-
-    local ok = pcall(utf_ptr2len, "🔭")
-    if not ok then
-      return fallback
-    end
-
-    return function(str, nchar, charlen)
-      local nbyte = 0
-      if nchar > 0 then
-        while nchar > 0 and nbyte < #str do
-          nbyte = nbyte + utf_ptr2len(str:sub(nbyte + 1))
-          nchar = nchar - 1
-        end
-      else
-        nbyte = nchar
-      end
-
-      local len = 0
-      if charlen then
-        while charlen > 0 and nbyte + len < #str do
-          local off = nbyte + len
-          if off < 0 then
-            len = len + 1
-          else
-            len = len + utf_ptr2len(str:sub(off + 1))
-          end
-          charlen = charlen - 1
-        end
-      else
-        len = #str - nbyte
-      end
-
-      if nbyte < 0 then
-        len = len + nbyte
-        nbyte = 0
-      elseif nbyte > #str then
-        nbyte = #str
-      end
-      if len < 0 then
-        len = 0
-      elseif nbyte + len > #str then
-        len = #str - nbyte
-      end
-
-      return str:sub(nbyte + 1, nbyte + len)
-    end
-  else
-    return fallback
-  end
-end)()
+M.strdisplaywidth = vim.fn.strdisplaywidth
 
 local truncate = function(str, len, dots, direction)
   if M.strdisplaywidth(str) <= len then
@@ -124,7 +18,7 @@ local truncate = function(str, len, dots, direction)
     end
   end
   while true do
-    local part = M.strcharpart(str, start, 1)
+    local part = vim.fn.strcharpart(str, start, 1)
     current = current + M.strdisplaywidth(part)
     if (current + len_of_dots) > len then
       result = concat(result, dots, direction)
